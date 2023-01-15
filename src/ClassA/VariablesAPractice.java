c691d1a6843e7c08704b3cbac9a9a897699b958d
Aish Shams <azshams@gmail.com>
2023-01-15 18:51:00 -0500

    add variabel

---
+++ src/ClassA/VariablesAPractice.java
@@ -0,0 +1,33 @@
+package ClassA;
+
+public class VariablesAPractice {
+    public static void main(String[] args) {
+
+
+        String name = "Aisha";
+        String lastName = "Shams";
+
+        int age = 30;
+
+        char gender = 'f';
+
+        long salary = 250000000000l;
+
+        String streetName="Clarendon Sq ashburn Va 20148";
+
+        String city= "ashburn";
+
+        String state="VA";
+
+        System.out.println(name);
+        System.out.println(lastName);
+        System.out.println(age);
+        System.out.println(gender);
+        System.out.println(salary);
+        System.out.println(streetName);
+        System.out.println(city);
+        System.out.println(state);
+
+
+    }
+}
